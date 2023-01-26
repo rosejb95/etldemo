aa30036d4da6d5088ce69afc270531dcfec31448
rosejb95 <james.benton.rose@gmail.com>
2023-01-26 12:47:37 -0600

    fixing code pull and sync

--- infrastructure/helm_charts/bdb-chart/box_user_setup.sh
+++ infrastructure/helm_charts/bdb-chart/box_user_setup.sh
@@ -1,3 +1,4 @@
 ssh-copy-id -i ~/.ssh/id_rsa.pub dev@localhost
 ssh dev@localhost "git config --global user.name $(git config --get user.name)"
 ssh dev@localhost "git config --global user.email $(git config --get user.email)"
+ssh dev@localhost 'cd ~/repo && git config --global --add safe.directory ~/repo && git remote set-url origin git@github.com:rosejb95/etldemo.git'
