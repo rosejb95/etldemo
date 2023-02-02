83de5f4aa2e5aaea3f3734543b4eb9584b7bf7e3
rosejb95 <james.benton.rose@gmail.com>
2023-02-02 21:48:26 +0000

    Formatted File

--- code/python/load_iris_to_sql.py
+++ code/python/load_iris_to_sql.py
@@ -1,15 +1,18 @@
 # %%
+
 import pandas as pd
 from sklearn.datasets import load_iris
-import pyodbc
 
 FILE_FOLDER = r"/mnt/etlshare"
 FILE_NAME = "iris_clean.pkl"
 
+# %%
+iris_df = load_iris(as_frame=True)['data']
+
+
+# %%
 
 def main():
-
-    # %%
     iris_df = load_iris(as_frame=True)['data']
     # %%
     iris_df.columns = iris_df.columns.str.replace(' (cm)', '', regex=False).str.replace('\s', '_')
