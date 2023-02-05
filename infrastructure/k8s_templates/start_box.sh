2a284404f6273e74b1a5f7385fa0f4487e631c9e
rosejb95 <james.benton.rose@gmail.com>
2023-02-05 16:17:34 +0000

    Setup runner.env for default job and pod behavior.

--- infrastructure/k8s_templates/start_box.sh
+++ infrastructure/k8s_templates/start_box.sh
@@ -1,18 +1,38 @@
-config_temp_file=$(mktemp)
 manifest_temp_file=$(mktemp)
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo "
-podName: $1
-branch: $2
-">$config_temp_file
+export $( cat $script_dir/runner.env | xargs )
+export image_tag=$DEFAULT_IMAGE_TAG
+export branch=$DEFAULT_BRANCH
 
-# Generate completed manifest.
-j2 --format=yaml "$script_dir/bdb_box_template.yml" $config_temp_file >$manifest_temp_file
+while getopts t:n:b:c:i: opt; do
+  case $opt in 
+    t) export image_tag=$OPTARG ;;
+    n) export name=$OPTARG ;;
+    b) export branch=$OPTARG ;;
+    c) export command=$OPTARG ;;
+    i) interactive=$OPTARG ;;
+    *) echo 'Unexpected argument found' >&2; exit 1
+  esac
+done
+
+if [ $interactive != 'yes' ] && [ -z command ];
+then
+  echo "Either -c (command) or -i (interactive) is required." >&2
+  exit 1
+fi
+
+if [ $interactive == 'yes' ];
+then
+  template_file_name="$script_dir/bdb_box_template.yml"
+else
+  template_file_name="$script_dir/bdb_job_template.yml"
+fi
+
+# Values are set via environment variables.
+j2 $template_file_name >$manifest_temp_file
 
 # Start the box -- TODO: check failure behavior.
 kubectl apply -f $manifest_temp_file
-kubectl wait -f $manifest_temp_file --for=condition=ready --timeout=300s
 
-#Start port-forwarding in background.
-kubectl port-forward pod/$1 22:22
+exit
